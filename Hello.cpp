201df4872cc9b67ddd262a3924ba19ba09e7d841
Firstoly <139094262+Firstoly@users.noreply.github.com>
2023-07-10 10:38:32 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"student code : 66-060216-31033 \n";
+   cout<<"student name : natchapak poim \n";
+   cout<<"division : information technology \n";
+   cout<<"year : 1 \n";
+   cout<<"room : rb \n";
+   cout<<"sex : male \n";
+   cout<<"age : 18 \n";
+ 
+   return 0;
+}
+	    
