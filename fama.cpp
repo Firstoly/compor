bf7175cffc039aa986a9c0b601a3eb69226b8ca7
Firstoly <139094262+Firstoly@users.noreply.github.com>
2023-07-17 11:52:07 +0700

    Add files via upload

---
+++ fama.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    float f;
+	cout << "Enter fahrenheit  :";
+	cin >> f ;
+	cout << "*****Ouput*****\n";
+	cout << "Celcius = "<< (5.0/9.0)*(f-32)<<endl;
+	system ("pause");
+	return 0;
+
+
+
+
+
+
+}
