e9eec8bd36bff97f82c0ef77f79971a15588206d
Firstoly <139094262+Firstoly@users.noreply.github.com>
2023-07-17 10:41:16 +0700

    Add files via upload

---
+++ one one/one one/one one.cpp
@@ -0,0 +1,25 @@
+#include <iostream>s
+#include <string>
+using namespace std;
+int main()
+{
+	string Name;
+	int yearBrith , yearNow = 2023;
+
+    cout << "Enter your name :" ;
+    cin  >> Name ;
+	cout << "Enter your year of bith :" ;
+	cin  >> yearBrith ;
+
+	cout << "\nYour age = " << yearNow - yearBrith <<" years old.\n\n" ;
+
+	system ("pause");
+	return 0;
+}
+
+
+	 
+
+
+
+
