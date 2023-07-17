cc0bede803f6bd428bf9777b94e5a03b1541fea5
Firstoly <139094262+Firstoly@users.noreply.github.com>
2023-07-17 11:54:48 +0700

    Add files via upload

---
+++ first one.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{ 
+	string Name, Sex;
+	int Age;
+	cout << "Enter name: ";
+	cin >> Name;
+	cout << "Enter age: ";
+	cin >> Age;
+	cout << "Enter sex: ";
+	cin >> Sex;
+	cout << "\nHello, "<< Name << "." << endl;
+	cout << "you have "<< Age << " year old." <<endl;
+	cout << "you are "<< Sex << "." << endl;
+	cout << "You are beginner programmer. "<< endl;
+	system ("pause");
+	return 0; */
+	
+
+}
